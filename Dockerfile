a5b73966df4e44e34e1e34ed8fd26a0da5eb50fd
Zyra Zia <zyra.zia@gmail.com>
2020-12-15 22:10:20 +0000

    Initial backup script

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM ubuntu:16.04
+
+# Install prerequisites
+RUN apt-get update && apt-get install -y curl && apt-get install -y sudo && apt-get install -y unzip && apt-get install -y wget
+
+# Install rclone
+RUN curl https://rclone.org/install.sh | sudo bash
+
+# Install setup script for rclone config
+RUN wget https://raw.githubusercontent.com/zyra-zia/keep-backups/main/setup-conf.sh
+
+RUN chmod +x setup-conf.sh && ./setup-conf.sh
+
--- setup-conf.sh
+++ setup-conf.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
 
+# Create a custom config file for rclone and fill with environment variables
+cat > user-rclone.conf <<EOF
+[remote]
+type = $TYPE
+account = $ACCOUNT
+key = $KEY
+hard_delete = false
+EOF
+
+# Create a daily backup script
+cat > daily-backup.sh <<EOF
+#!/bin/sh
+
+#Daily Backups
+rclone copy /persistence remote:bucket/daily/current --backup-dir remote:bucket/daily/`date -I` --create-empty-src-dirs --config /user-rclone.conf
+echo "$(date) backed up." >> /home/user/logs/sync.log
+EOF
+
+# Create a sync script
+cat > daily-backup.sh <<EOF
+#!/bin/sh
+
+rclone sync /persistence remote:bucket/sync --exclude 'keystore' --exclude 'trace.json' --exclude '.*{/**,}' --local-no-check-updated --create-empty-src-dirs --config /user-rclone.conf
+echo "$(date) data synced" >> /home/user/logs/sync.log
+EOF
